aa1a2cd51483633e8eb10b8f1de494767e1c7b42
Danya <danya001406@gmail.com>
2024-09-14 21:51:08 +0300

    Update and rename Project3.sln to simple_calc.cpp

---
+++ simple_calc.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+int main() {
+	setlocale(LC_ALL, "RU");
+
+	float num1, num2;
+	std::cout << "Введите первое число: ";
+	std::cin >> num1;
+	std::cout << "Введите второе число: ";
+	std::cin >> num2;
+	char math;
+	std::cout << "Введите действие с числами: ";
+	std::cin >> math;
+	switch (math)
+	{
+	case '+':
+		std::cout << "Результат: " << num1 << " + " << num2 << " = " << num1 + num2 << std::endl; break;
+	case '-': 
+		std::cout << "Результат: " << num1 << " - " << num2 << " = " << num1 - num2 << std::endl; break;
+	case '*':
+		std::cout << "Результат: " << num1 << " * " << num2 << " = " << num1 * num2 << std::endl; break;
+	case '/':
+		if (num2 != 0) {
+			std::cout << "Результат: " << num1 << " / " << num2 << " = " << num1 / num2 << std::endl; break;
+		}
+		else {
+			std::cout << "Ошибка: Делить на ноль нельзя" << std::endl; break;
+		}
+	default:
+		std::cout << "Error" << std::endl;
+	}
+	return 0;
+}
+
